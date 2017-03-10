390e71824e2bec4fd5bd3bc2b076083c1b75e3ff
mauricefreese <mauricefreese@amdg.rockhursths.edu>
2017-03-10 11:34:17 -0600

    three

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var bodyParser = require('body-parser');
-var express = require('express');
+var express = require('express')
 
-var app = express();
+var app = express()
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -9,15 +9,16 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.send('Hello World!')
 });
 
+
 app.post('/', function (req, res) {
-    console.log('post / = ' + JSON.stringify(req.body));
+    console.log('post / = ' + req.body);
     res.status(200).send('got it');
 });
 
+
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+    console.log('Example app listening on port:3000!')
 });
-Contact GitHub API Training Shop Blog About
