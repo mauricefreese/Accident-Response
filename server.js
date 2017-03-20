1a26c0089d84ebaa068093cbafdd5909dfb948e0
mauricefreese <mauricefreese@amdg.rockhursths.edu>
2017-03-20 11:25:15 -0500

    m2,3/20/17

--- server.js
+++ server.js
@@ -8,3 +8,15 @@ app.get('/', function (req, res) {
 app.listen(3000, function () {
     console.log('Example app listening on port:3000!')
 });
+
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.post('/', function (req, res) {
+    console.log('post / = ' + req.body);
+    res.status(200).send('got it');
+});
