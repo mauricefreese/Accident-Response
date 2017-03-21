77be0c4b92169d1cc09c86bd896ff302c538ee71
mauricefreese <mauricefreese@amdg.rockhursths.edu>
2017-03-21 11:45:48 -0500

    m3/21/17

--- server.js
+++ server.js
@@ -1,22 +1,20 @@
-var express = require('express')
-var app = express()
-
-app.get('/', function (req, res) {
-    res.send('Hello World!')
-});
-
-app.listen(3000, function () {
-    console.log('Example app listening on port:3000!')
-});
-
 var bodyParser = require('body-parser');
+var express = require('express')
+
+var app = express()
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 
+app.get('/', express.static('../WebClient/'));
+
 app.post('/', function (req, res) {
-    console.log('post / = ' + req.body);
+    console.log('post / = ' + JSON.stringify(req.body));
     res.status(200).send('got it');
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port:3000!')
 });
