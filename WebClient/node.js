a49e069f54103d90899dbcf864f58e4fd98a75db
Darius Washington <dariuswashington@amdg.rockhursths.edu>
2017-03-22 10:59:38 -0500

    D3/22/17

---
+++ WebClient/node.js
@@ -0,0 +1,6 @@
+app.get('/', function (req, res) {
+    res.send('Are you in need?');
+    
+    <link rel="stylesheet" hyref="https://cdnjs.cloudflare.com/ajax/libs/twitter-boostrap">
+        <
+});
