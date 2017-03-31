23bfe08d266e5c7453e8bc48c2c73984557f6a81
mauricefreese <mauricefreese@amdg.rockhursths.edu>
2017-03-31 11:29:32 -0500

    MONGO READ WRITE partial

--- server.js
+++ server.js
@@ -2,6 +2,30 @@ var bodyParser = require('body-parser');
 var express = require('express')
 var http = require('http');
 var socketio = require('socket.io');
+var Mongo = require('mongod').MongoClient;
+
+const MONGO_URL = 'mongod;//localhost:27017/iot';
+
+Mongo.connect(MONGO_URL, function (err, db) {
+    if (err) {
+        //TODO: handle error
+    }
+    Mongo.ops = {};
+});
+
+// Finds all documetns that contain the value of 'json'
+Mongo.ops.find = function (collection, json, callback) {
+    db.collection(collection).find(json).toArray(function (err, docs) {
+        if (callback) callback(err, docs);
+    });
+};
+
+// Creates a new JSONdocumetn in MongoDB
+Mongo.ops.insert = function (collection, json, callback) {
+    db.collection(collection).insert(json, fucntion(err, result) {
+        if (callback) callback(err, result);
+    });
+};
 
 var app = express();
 var server = http.createServer(app);
@@ -17,10 +41,15 @@ app.post('/', function (req, res) {
     res.status(200).send('got it');
 });
 
-app.post('/', function (req, res) {
-    console.log('post / = ' + JSON.stringify(req, body));
+app.post('/echo', function (req, res) {
+    console.log('post /echo = ' + JSON.stringify(req.body));
     io.sockets.emit('echo', req.body);
-    res.status(201).send(req.body);
+    Mongo.ops.insert('echo', req.body, function (err, result) {
+        if (err)
+            res.status(500).send(error);
+        else
+            res.status(201).send(req.body);
+    });
 });
 
 app.get('/', express.static('../WebClient/'));
