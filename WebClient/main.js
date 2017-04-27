e583097a7027e044eaf88febec87ab72c9e0bc37
mauricefreese <mauricefreese@amdg.rockhursths.edu>
2017-04-27 11:38:27 -0500

    temp working only

--- WebClient/main.js
+++ WebClient/main.js
@@ -1,10 +1,10 @@
-$(document).ready(function() {
-  var socketServer = 'http://localhost:3000/';
-  var scoket = io.connect(socketServer);
-  
-  socket.on('echo', function(data) {
-    console.log('socket echo = ' + JSON.stringify(data));
-    $('#dht-display').text(data.temp +"C, " + data.humi + "%");
-    $('range-display').text(data.range);
-  });
+$(document).ready(function () {
+    var socketServer = 'http://localhost:3000';
+    var socket = io.connect(socketServer);
+
+    socket.on('echo', function (data) {
+        console.log('socket echo = ' + JSON.stringify(data));
+        $('#dht-display').text(data.temp + "C, " + data.humi + "%");
+        $('range-display').text(data.range);
+    });
 });
--- server.js
+++ server.js
@@ -2,32 +2,6 @@ var bodyParser = require('body-parser');
 var express = require('express')
 var http = require('http');
 var socketio = require('socket.io');
-var Mongo = require('mongodb').MongoClient;
-
-const MONGO_URL = 'mongodb://localhost:27017/iot';
-
-Mongo.connect(MONGO_URL, function (err, db) {
-    if (err) {
-        //TODO: handle error
-    }
-    Mongo.ops = {};
-
-    // Finds all documetns that contain the value of 'json'
-    Mongo.ops.find = function (collection, json, callback) {
-        db.collection(collection).find(json).toArray(function (err, docs) {
-            if (callback) callback(err, docs);
-        });
-    };
-
-    // Creates a new JSONdocumetn in MongoDB
-    Mongo.ops.insert = function (collection, json, callback) {
-        db.collection(collection).insert(json, function (err, result) {
-            if (callback) callback(err, result);
-        });
-    };
-});
-
-
 
 var app = express();
 var server = http.createServer(app);
@@ -46,16 +20,11 @@ app.post('/', function (req, res) {
 app.post('/echo', function (req, res) {
     console.log('post /echo = ' + JSON.stringify(req.body));
     io.sockets.emit('echo', req.body);
-    Mongo.ops.insert('echo', req.body, function (err, result) {
-        if (err)
-            res.status(500).send(error);
-        else
-            res.status(201).send(req.body);
-    });
+    res.status(200).send('ok');
 });
 
-app.get('/', express.static('../WebClient/'));
+app.use('/', express.static(__dirname + '/WebClient'));
 
-app.listen(3000, function () {
-    console.log('Example app listening on port:3000!')
+server.listen(3000, function () {
+    console.log('port:3000 listening')
 });
